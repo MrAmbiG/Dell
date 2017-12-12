aa4e0ee487647f6ea59137086a559c6fb6e5336b
Gajendra D Ambi <gajendra.ambi@emc.com>
2017-12-12 16:34:53 +0530

    files

---
+++ idrac/idracRootPassChange.ps1
@@ -0,0 +1,38 @@
+﻿<#
+.SYNOPSIS
+    Resets the idrac's root password
+.DESCRIPTION
+    opens up a csv file. put the ip addresses of idrac, old password and new password
+    in their respective columns.
+.NOTES
+    File Name      : idracRootPassChange.ps1
+    Author         : gajendra d ambi
+    Date           : Dec 2017
+    Prerequisite   : PowerShell v4+, Dell OpenManage DRAC Tools, includes Racadm (64bit) v8.1 or higher
+    Copyright      - None
+.LINK
+    Script posted over: github.com/gajuambi/vmware
+#>
+
+#Start of Script
+
+Write-Host "
+A CSV file will be opened (open in excel/spreadsheet)
+populate the values,
+save & close the file,
+Hit Enter to proceed
+" -ForegroundColor Blue -BackgroundColor White
+
+$csv = "$PSScriptRoot/idracRootPassChange.csv"
+get-process | Select-Object idrac_ip_address, currentPassword, NewPassword| Export-Csv -Path $csv -Encoding ASCII -NoTypeInformation
+Start-Process $csv
+Read-Host "Hit Enter/Return to proceed"
+Write-Host "processing your entries from the csv file...."
+$csv = Import-Csv $csv
+foreach ($line in $csv) 
+ {
+    $idrac_ip_address = $($line.idrac_ip_address)
+    $currentPassword = $($line.currentPassword)
+    $NewPassword = $($line.NewPassword)
+    racadm -r $idrac_ip_address -u "root" -p $currentPassword set iDRAC.Users.2.Password $NewPassword
+ }
---
+++ idrac/idracSetHostname.ps1
@@ -0,0 +1,38 @@
+﻿<#
+.SYNOPSIS
+    set the hostname for dell iDRAC servers
+.DESCRIPTION
+    opens up a csv file. put the ip addresses of idrac, hostname to be set
+.NOTES
+    File Name      : idracSetHostname.ps1
+    Author         : gajendra d ambi
+    Date           : Dec 2017
+    Prerequisite   : PowerShell v4+, Dell OpenManage DRAC Tools, includes Racadm (64bit) v8.1 or higher
+    Copyright      - None
+.LINK
+    Script posted over: github.com/MrAmbiG/
+#>
+
+#Start of Script
+
+Write-Host "
+A CSV file will be opened (open in excel/spreadsheet)
+populate the values,
+save & close the file,
+Hit Enter to proceed
+" -ForegroundColor Blue -BackgroundColor White
+
+$csv = "$PSScriptRoot/idracRootPassChange.csv"
+get-process | Select-Object idrac_ip_address, root_password, hostname | Export-Csv -Path $csv -Encoding ASCII -NoTypeInformation
+Start-Process $csv
+Read-Host "Hit Enter/Return to proceed"
+Write-Host "processing your entries from the csv file...."
+$csv = Import-Csv $csv
+foreach ($line in $csv) 
+ {
+    $idrac_ip_address = $($line.idrac_ip_address)
+    $root_password = $($line.root_password)
+    $hostname = $($line.hostname)
+    Write-Host "setting $hostname as hostname on $idrac_ip_address"
+    racadm -r $idrac_ip_address -u "root" -p $root_password set iDRAC.NIC.DNSDomainName $domain    
+ }
